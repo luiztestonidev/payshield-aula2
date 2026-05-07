f499621f4af4a0f534f5dd7c2522b3380e737504
luiztestonidev <luiztestoni5@gmail.com>
2026-05-07 20:42:45 -0300

    feat: adicionar classe processador pagamento

--- src/main/java/org/example/ProcessadorPagamento.java
+++ src/main/java/org/example/ProcessadorPagamento.java
@@ -1,4 +1,26 @@
 package org.example;
 
 public class ProcessadorPagamento {
+
+     private CartaoCredito cartao;
+
+     public ProcessadorPagamento(CartaoCredito cartao) {
+         this.cartao = cartao;
+     }
+
+     public void executarVenda(double valor) {
+         if (valor <= cartao.getLimiteDisponivel()) {
+             cartao.alterarLimite(cartao.getLimiteDisponivel() - valor);
+             System.out.println("Venda de R$ " + valor + " aprovada");
+             System.out.println("limite restante R$ " + cartao.getLimiteDisponivel());
+         } else {
+             System.out.println("Transação negada, limite insuficiente.");
+         }
+     }
+     
+     public void estornarVenda(double valor) {
+         cartao.alterarLimite(cartao.getLimiteDisponivel() + valor);
+         System.out.println("Estorno de R$ " + valor + " realizado");
+         System.out.println("Limite restante R$ " + cartao.getLimiteDisponivel());
+     }
 }
