bac1d66afd1efd6b7c73405e96b76e2502ebf9a0
luiztestonidev <luiztestoni5@gmail.com>
2026-05-07 21:09:22 -0300

    feat: poo adiciona main com todas as classes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,14 @@ package org.example;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static void main(String[] args) {
 
+        CartaoCredito cartao = new CartaoCredito("1234-5678-9101-1121", "luiz", 5000.00);
+
+        ProcessadorPagamento processador = new ProcessadorPagamento(cartao);
+
+        processador.executarVenda(2000.00);
+
+        processador.executarVenda(4000.00);
+    }
 }
